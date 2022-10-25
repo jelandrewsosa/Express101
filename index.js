a282411e311dab92cd02f8819f0c5b3889bade9f
jelandrewsosa <107853804+jelandrewsosa@users.noreply.github.com>
2022-10-25 20:25:07 +0800

    feat: initial commit
    add a simple express server using get and post

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+
+  if (users.length === 0) {
+    return res.status(400).json({
+      error: "Users is Empty",
+    });
+  } else {
+    res.status(200).json(users);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const details = req.body;
+  const filteredDetails = Object.fromEntries(
+    Object.entries(details).filter(
+      ([key]) =>
+        key.includes("firstName") ||
+        key.includes("lastName") ||
+        key.includes("email")
+    )
+  );
+  users.push(filteredDetails);
+  return res.status(201).json(filteredDetails);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
